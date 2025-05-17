695790e3d39fca784e0fccd1e8ff11665e02495c
yusokolov <yu50k0107@gmail.com>
2025-05-17 21:18:04 +0300

    Корректировка кода index.html и app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["get", "post"])
+@app.route('/', methods=["get", "post"]) # 127.0.0.1:5000/
 def index():
     message = "Стартовое сообщение"
     if request.method == "POST":
         area = request.form.get(area)
         # проверка кода
-        area = float(area)
-    return render_tamplate("index.html", message = message)
+        print(area)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
