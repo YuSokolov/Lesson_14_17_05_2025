2cba9cc8fe48cc66eebcb2300eb5b5399fd9dffd
yusokolov <yu50k0107@gmail.com>
2025-05-17 23:15:10 +0300

    Отображение результатов расчета

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=["get", "post"]) # 127.0.0.1:5000/
 def index():
-    # message = "Стартовое сообщение"
+    message = "Здесь будут отражены результаты прогноза"
     if request.method == "POST":
         area = request.form.get("area")
         # проверка кода
         cost = get_prediction(float(area))
-        print(cost)
+        message = f"Стоимость квартиры {area} кв.м. составляет {cost} руб."
 
-    return render_template("index.html")
+    return render_template("index.html", message = message)
 
 if __name__ == '__main__':
     app.run()
