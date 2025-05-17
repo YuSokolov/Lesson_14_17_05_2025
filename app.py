254e5e00910cebafbb9d35154abf2113bacfb137
yusokolov <yu50k0107@gmail.com>
2025-05-17 21:34:42 +0300

    Корректировка кода app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=["get", "post"]) # 127.0.0.1:5000/
 def index():
-    message = "Стартовое сообщение"
+    # message = "Стартовое сообщение"
     if request.method == "POST":
-        area = request.form.get(area)
+        area = request.form.get("area")
         # проверка кода
         print(area)
     return render_template("index.html")
