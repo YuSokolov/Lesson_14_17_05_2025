dcbb51486a3c30496e51889a543c47faf896a446
yusokolov <yu50k0107@gmail.com>
2025-05-18 20:41:45 +0300

    Доработка обработчика для загрузки ранее обученной модели, данных пользователей и определения возрастной группы по номеру пользователя. Добавлены файлы с моделью и данными пользователей.

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ def index():
 
     return render_template("index.html", message = message)
 
-#if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+    app.run()
--- processing.py
+++ processing.py
@@ -1,5 +1,15 @@
 from keras import models
 import numpy as np
 
-def get_prediction(area):
-    return 300000 * area
+def get_prediction(user_num):
+    
+    # Загрузка ранее обученной модели
+    saved_model = models.load_model("model_age.keras")
+    # Загрузка данных пользователей
+    user_data_from_file = np.genfromtxt("X_age_test_np.csv", delimiter=",")
+    
+    # предсказания по возрасту из сохраненной модели
+    saved_predictions = saved_model.predict(user_data_from_file)
+    
+    # Поиск возрастной группы пользователя
+    return np.argmax(saved_predictions[user_num])
